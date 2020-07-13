0d2dcedc4a215b23c10e460de84845287782832b
Marxneves <marxkhristie@gmail.com>
2020-07-13 17:56:27 -0300

    Segundo exercicio da atividade resolvido.

---
+++ Blocos/4.1/exer_2.js
@@ -0,0 +1,8 @@
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+let a = 5;
+let b = 7;
+
+if(a>b)
+  console.log(a);
+else
+  console.log(b);
