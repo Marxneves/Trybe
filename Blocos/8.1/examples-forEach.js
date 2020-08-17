8b0d6e670b8fac0aae9042d6b73fb2f3cafba8da
Marxneves <marxkhristie@gmail.com>
2020-08-17 15:41:53 -0300

    Testando exemplos do conteudo

---
+++ Blocos/8.1/examples-forEach.js
@@ -0,0 +1,31 @@
+const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+const printValues = arrayOfValues.forEach((element, indexOfTheArray, theEntireArray) => {
+  console.log('Cada elemento do array:', element);
+  console.log('Index, posição do elemento:', indexOfTheArray);
+  console.log('Array percorrido:', theEntireArray);
+});
+
+// => Output
+//   Cada elemento do array: josé
+//   Index, posição do elemento: 0
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: 50
+//   Index, posição do elemento: 1
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: 0.25
+//   Index, posição do elemento: 2
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+//   ---------------
+//   Cada elemento do array: { comida: 'Chocolate' }
+//   Index, posição do elemento: 3
+//   Array percorrido: [ 'josé', 50, 0.25, { comida: 'Chocolate' } ]
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const multipliesFor2 = (element) => {
+  console.log(`${element} * 2: ${element * 2}`);
+};
+
+numbers.forEach(multipliesFor2);
