5679718d2d350bf68de573a22e6f3befbb6d76b7
Marxneves <marxkhristie@gmail.com>
2020-08-19 16:06:31 -0300

    Exercicio 4 finalizado

---
+++ Blocos/8.3/exerc5.js
@@ -0,0 +1,17 @@
+    
+const assert = require('assert');
+
+const names = [
+  'Aanemarie',  'Adervandes',   'Akifusa',
+  'Abegildo',   'Adicellia',    'Aladonata',
+  'Abeladerco', 'Adieidy',  'Alarucha',
+];
+
+
+
+function containsA() {
+  return names.reduce((acumulador, atual) => {
+    return acumulador += (atual.toLowerCase().split('').reduce((acc, string) => (string === 'a')? acc += 1 : acc ,0))},0)
+}
+
+assert.deepEqual(containsA(), 20);
