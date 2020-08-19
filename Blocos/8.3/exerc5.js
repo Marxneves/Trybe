25cbcd83cc6b39ad12d76c8a466546254408cb98
Marxneves <marxkhristie@gmail.com>
2020-08-19 16:08:21 -0300

    Exercicio 5 resolvido

--- Blocos/8.3/exerc5.js
+++ Blocos/8.3/exerc5.js
@@ -7,8 +7,6 @@ const names = [
   'Abeladerco', 'Adieidy',  'Alarucha',
 ];
 
-
-
 function containsA() {
   return names.reduce((acumulador, atual) => {
     return acumulador += (atual.toLowerCase().split('').reduce((acc, string) => (string === 'a')? acc += 1 : acc ,0))},0)
