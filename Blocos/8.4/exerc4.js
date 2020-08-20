40faecb1db0276af006623988521799d10279a30
Marxneves <marxkhristie@gmail.com>
2020-08-20 15:47:01 -0300

    commitando exercicios do bloco 8.4 resolvidos

---
+++ Blocos/8.4/exerc4.js
@@ -0,0 +1,43 @@
+const assert = require('assert')
+
+const people = [
+    {
+        name: "Nicole",
+        bornIn: 1992,
+        nationality: "Australian"
+    },
+    {
+        name: "Harry",
+        bornIn: 2008,
+        nationality: "Australian"
+    },
+    {
+        name: "Toby",
+        bornIn: 1901,
+        nationality: "Australian"
+    },
+    {
+        name: "Frida",
+        bornIn: 1960,
+        nationality: "Dannish"
+    },
+    {
+        name: "Fernando",
+        bornIn: 2001,
+        nationality: "Brazilian"
+    }
+]
+
+// escreva filterPeople abaixo
+const filterPeople = (objeto) => {
+  return objeto.filter((obj) => { 
+    const {name, bornIn, nationality} = obj
+    if(bornIn > 1900 && bornIn < 2001 && nationality === 'Australian')
+    return {name, bornIn, nationality};
+  })
+}
+
+const filteredPeople = filterPeople(people)
+
+assert.deepEqual(filteredPeople[0], { name: "Nicole", bornIn: 1992, nationality: "Australian" })
+assert.deepEqual(filteredPeople[1], { name: "Toby", bornIn: 1901, nationality: "Australian" })
