752bd9aca75bacefccba0b6869b27a042e9cb11e
Marxneves <marxkhristie@gmail.com>
2020-09-02 18:58:03 -0300

    Exercicio 1 de Jest

--- Blocos/10.1/jest/exerc1.test.js
+++ Blocos/10.1/jest/exerc1.test.js
@@ -1,7 +1,6 @@
 function sum(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
-    // throw new Error('parameters must be numbers');
-    return 'parameters must be numbers';
+    throw new Error('parameters must be numbers');
   }
 
   return a + b;
@@ -11,7 +10,7 @@ describe('This is Sum Function', () => {
   it('Sum two numbers', () => {
     expect(sum(4, 5)).toBe(9);
     expect(sum(0, 0)).toBe(0);
-    expect(typeof (sum(4, '5'))).toBe('string');
-    expect(sum(4, '5')).toBe('parameters must be numbers');
+    expect(() => sum(4, '5')).toThrow();
+    expect(() => sum(4, '5')).toThrow(/parameters must be numbers/);
   });
 });
