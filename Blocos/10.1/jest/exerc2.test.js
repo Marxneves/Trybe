1edd14ef65f8b66f9963fa26e50a2c4af507fbf8
Marxneves <marxkhristie@gmail.com>
2020-09-02 18:58:23 -0300

    Exercicio 2 de Jest

--- Blocos/10.1/jest/exerc2.test.js
+++ Blocos/10.1/jest/exerc2.test.js
@@ -9,8 +9,10 @@ function myRemove(arr, item) {
 }
 
 describe('Test function myRemove', () => {
+  const array = [1, 2, 3, 4];
   it('Remove item from Array', () => {
-    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
-    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    expect(myRemove(array, 3)).toEqual([1, 2, 4]);
+    expect(myRemove(array, 3)).not.toEqual([1, 2, 3, 4]);
+    expect(myRemove(array, 5)).toEqual([1, 2, 3, 4]);
   });
 });
