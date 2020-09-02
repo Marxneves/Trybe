756947f460b5fe83ff61414eec36408e2ce1efe1
Marxneves <marxkhristie@gmail.com>
2020-09-02 18:57:34 -0300

    Atualizacao do README.md

--- Blocos/10.1/jest/exerc1.test.js
+++ Blocos/10.1/jest/exerc1.test.js
@@ -1,6 +1,7 @@
 function sum(a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
-    throw new Error('parameters must be numbers');
+    // throw new Error('parameters must be numbers');
+    return 'parameters must be numbers';
   }
 
   return a + b;
@@ -10,6 +11,7 @@ describe('This is Sum Function', () => {
   it('Sum two numbers', () => {
     expect(sum(4, 5)).toBe(9);
     expect(sum(0, 0)).toBe(0);
-    // expect(sum(4, '5')).toBe('parameters must be numbers');
+    expect(typeof (sum(4, '5'))).toBe('string');
+    expect(sum(4, '5')).toBe('parameters must be numbers');
   });
 });
---
+++ Blocos/10.1/jest/exerc2.test.js
@@ -0,0 +1,16 @@
+function myRemove(arr, item) {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+describe('Test function myRemove', () => {
+  it('Remove item from Array', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+});
