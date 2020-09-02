1723f025074936ff97530ce14560b12666b18595
Marxneves <marxkhristie@gmail.com>
2020-09-02 16:45:58 -0300

    Exerc 1 jest test

---
+++ Blocos/10.1/jest/exerc1.test.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+describe('This is Sum Function', () => {
+  it('Sum two numbers', () => {
+    expect(sum(4, 5)).toBe(9);
+    expect(sum(0, 0)).toBe(0);
+    // expect(sum(4, '5')).toBe('parameters must be numbers');
+  });
+});
